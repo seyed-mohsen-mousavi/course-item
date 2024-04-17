fa641e05d6ad10505c9b7423fd4ce10d52b29188
mr.root <1mr.rooter3@gmail.com>
2024-04-17 18:02:51 +0330

    hey Broo

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
